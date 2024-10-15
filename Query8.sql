2141d9dabc67f6de4cb72d139346751063dac343
vinaldsz <dsouza.vi@northeastern.edu>
2024-10-14 20:10:20 -0700

    Updating query8 with projection of playlist name

--- Query8.sql
+++ Query8.sql
@@ -1,7 +1,7 @@
 --Query8 : Returns playlistid and the total time in a playlist rouned to 2 decimal places
-SELECT PlaylistId, ROUND(sum(Milliseconds)/(1.0 *3600000),2)total_length_in_hrs
-FROM playlists
+SELECT PlaylistId, a.Name,ROUND(sum(Milliseconds)/(1.0 *3600000),2)total_length_in_hrs
+FROM playlists a
 JOIN playlist_track USING (PlaylistId)
 JOIN tracks USING (TrackId)
-GROUP BY PlaylistId
+GROUP BY PlaylistId,a.Name
 HAVING total_length_in_hrs > 2.0;
